ecf73acc71c1148d0f90e8b49a04852f0e0520c5
jeje <le-berre.jeremy@orange.fr>
2020-12-07 20:18:59 +0100

    ajout theme

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,44 @@ class HomeController extends AbstractController
     {
         return $this->render('home/theme1.html.twig');
     }
+
+      /**
+     * @Route("/theme-2", name="theme2")
+     */
+    public function theme2(): Response
+    {
+        return $this->render('home/theme2.html.twig');
+    }
+
+      /**
+     * @Route("/theme-3", name="theme3")
+     */
+    public function theme3(): Response
+    {
+        return $this->render('home/theme3.html.twig');
+    }
+
+      /**
+     * @Route("/theme-4", name="theme4")
+     */
+    public function theme4(): Response
+    {
+        return $this->render('home/theme4.html.twig');
+    }
+
+      /**
+     * @Route("/theme-5", name="theme5")
+     */
+    public function theme5(): Response
+    {
+        return $this->render('home/theme5.html.twig');
+    }
+
+      /**
+     * @Route("/theme-6", name="theme6")
+     */
+    public function theme6(): Response
+    {
+        return $this->render('home/theme6.html.twig');
+    }
 }
